06cebab5596af66b2a0ffcdecb774ffa5415909d
macpro <c915384165@outlook.com>
2020-08-26 21:56:58 +0800

    add init

---
+++ init.el
@@ -0,0 +1,66 @@
+;;(cd "/Users/macpro/")
+;;(add-hook 'find-file-hook #'(lambda () (setq default-directory (expand-file-name "/Users/macpro/"))))
+;;(setenv "HOME" "~/")
+
+;;(setq default-directory "/Users/macpro/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;global set keys
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C-x C-f to ~/
+(defun my-find-file ()
+  (interactive)
+  (let ((default-directory "/Users/macpro/"))
+    (call-interactively #'find-file)))
+(global-set-key (kbd "C-x C-f") 'my-find-file)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; f5 = open init.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun open-my-init-file()
+ (interactive)
+ (find-file "/Users/macpro/.emacs.d/init.el"))
+(global-set-key (kbd "<f5>") 'open-my-init-file)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; f6 = auto-fill-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "<f6>") 'auto-fill-mode)
+(setq default-fill-column 85)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; f7 = flyspell-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flyspell-mode
+(setq ispell-program-name "/usr/local/bin/ispell")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "<f7>") 'flyspell-mode)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key (kbd "<f8>") 'eval-buffer)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 滚动栏去掉
+(scroll-bar-mode nil)
+;; 行号
+(setq line-number-mode t)
+(setq column-number-mode t)
+;; 设置缩进
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
